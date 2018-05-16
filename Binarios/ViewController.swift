c8014163575557f406699e5eb2f36ae0bcc2abb8
Juan Morillo <juanmorillios@gmail.com>
2018-05-16 22:30:13 +0200

    Implementación de la UI + conexión del código con la interface + codificación del comportamiento del TextField para cuando no haya ningún dato.

--- Binarios/ViewController.swift
+++ Binarios/ViewController.swift
@@ -9,17 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var valueEntryTextField: UITextField!
+    @IBOutlet weak var binaryBtn: UIButton!
+    @IBOutlet weak var decimalBtn: UIButton!
+    
+    let placeHolder = NSAttributedString(string: "Escribe un #...", attributes: [NSAttributedStringKey.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), NSAttributedStringKey.font: UIFont(name: "Menlo", size: 36.0)!])
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        valueEntryTextField.attributedPlaceholder = placeHolder
+        valueEntryTextField.addTarget(self, action: #selector(textFieldTextDidChang), for: .editingChanged)
+        disableBtns()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc func textFieldTextDidChang() {
+        if valueEntryTextField.text == "" {
+            disableBtns()
+        } else {
+            enableBtns()
+        }
     }
-
-
+    
+    func disableBtns() {
+        binaryBtn.isEnabled = false
+        binaryBtn.alpha = 0.5
+        decimalBtn.isEnabled = false
+        decimalBtn.alpha = 0.5
+    }
+    
+    func enableBtns() {
+        binaryBtn.isEnabled = true
+        binaryBtn.alpha = 1.0
+        decimalBtn.isEnabled = true
+        decimalBtn.alpha = 1.0
+    }
+    
+    @IBAction func binaryBtnWasPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func decimalBtnWasPressed(_ sender: Any) {
+    }
+    
 }
 
